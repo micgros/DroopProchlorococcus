be7db5f4c0dfcd9758e15fd9255e045501cd8e47
micgros <micgros@gmail.com>
2017-10-27 15:16:21 +0300

    Fixing B_C function

--- Excretion model/autoCbiomass_excretion.m
+++ Excretion model/autoCbiomass_excretion.m
@@ -3,7 +3,7 @@ function [ dba_c_dt ] = autoCbiomass_excretion(V_c_max, C, k_c, x, q_c, q_c_max,
 % MG 12.7.12
 % last modification: 15 Feb 16, MG
 
-dba_c_dt = (V_c_max * C * x * (q_c_max - q_c)) / ((C + k_c) * (q_c_max - q_c_min)) + ...
+dba_c_dt = (V_c_max * C * x * (q_c_max - q_c)) / ((C + k_c) * (q_c_max - q_c_min)) - ...
     m * b_c - (r0 + b * mu) * b_c -epsilon_c*b_c;
 
 end
--- Sensetivity analysis/autoCbiomass.m
+++ Sensetivity analysis/autoCbiomass.m
@@ -3,7 +3,7 @@ function [ dba_c_dt ] = autoCbiomass(V_c_max, C, k_c, x, q_c, q_c_max, q_c_min, 
 % MG 12.7.12
 % last modification: 15 Feb 16, MG
 
-dba_c_dt = (V_c_max * C * x * (q_c_max - q_c)) / ((C + k_c) * (q_c_max - q_c_min)) + ...
+dba_c_dt = (V_c_max * C * x * (q_c_max - q_c)) / ((C + k_c) * (q_c_max - q_c_min)) - ...
     m * b_c - (r0 + b * mu) * b_c;
 
 end
